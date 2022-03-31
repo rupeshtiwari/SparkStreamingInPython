a544faaa23924132fa2ffa09909014adb435d468
Rupesh Tiwari <roopkt@gmail.com>
2022-03-30 22:51:19 -0400

    new work

--- KafkaStreamDemo.py
+++ KafkaStreamDemo.py
@@ -1,5 +1,13 @@
+"""
+Delete `chk-point-dir, kafka-logs, output` folders to start from scratch
+1. Run the Kafka-scripts step by step in sequence
+2. Next run this file
+3. In producer terminal enter the data/samples.json file json data one by one
+4. Wait for minute and check the output folder, it should have json output
+"""
+
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import col, from_json
+from pyspark.sql.functions import col, from_json, expr
 from pyspark.sql.types import StructType, StructField, StringType, LongType, DoubleType, IntegerType, ArrayType
 
 if __name__ == "__main__":
@@ -41,11 +49,36 @@ if __name__ == "__main__":
     ])
 
     # Read
-    kafka_df = spark.read \
+    kafka_df = spark.readStream \
         .format("kafka") \
         .option("kafka.bootstrap.servers", "localhost:9092") \
         .option("subscribe", "invoices") \
         .option("startingOffsets", "earliest") \
         .load()
 
-    value_df = kafka_df.select(from_json(col("value").cast("string"), schema)).show()
+    value_df = kafka_df.select(from_json(col("value").cast("string"), schema).alias("value"))
+    # flatten the dataframe
+    explode_df = value_df.selectExpr("value.InvoiceNumber", "value.CreatedTime", "value.StoreID",
+                                     "value.PosID", "value.CustomerType", "value.PaymentMethod", "value.DeliveryType",
+                                     "value.DeliveryAddress.City",
+                                     "value.DeliveryAddress.State", "value.DeliveryAddress.PinCode",
+                                     "explode(value.InvoiceLineItems) as LineItems")
+    flattened_df = explode_df \
+        .withColumn("ItemCode", expr("LineItems.ItemCode")) \
+        .withColumn("ItemDescription", expr("LineItems.ItemDescription")) \
+        .withColumn("ItemPrice", expr("LineItems.ItemPrice")) \
+        .withColumn("ItemQty", expr("LineItems.ItemQty")) \
+        .withColumn("TotalValue", expr("LineItems.TotalValue")) \
+        .drop("LineItems")
+
+    # write
+    invoice_writer_query = flattened_df.writeStream \
+        .format("json") \
+        .queryName("Flattened Invoice Writer") \
+        .outputMode("append") \
+        .option("path", "output") \
+        .option("checkpointLocation", "chk-point-dir") \
+        .trigger(processingTime="1 minute") \
+        .start()
+
+    invoice_writer_query.awaitTermination()
